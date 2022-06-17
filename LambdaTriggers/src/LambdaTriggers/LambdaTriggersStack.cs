c9441c77cc00d1bd4b88bf6d4ef33d7702e2a7a8
Scottie Enriquez <scottie@enriquez.xxx>
2022-06-17 13:10:17 -0700

    WIP: testing CDK unit test bug workaround

---
+++ LambdaTriggers/src/LambdaTriggers/LambdaTriggersStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace LambdaTriggers
+{
+    public class LambdaTriggersStack : Stack
+    {
+        internal LambdaTriggersStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
